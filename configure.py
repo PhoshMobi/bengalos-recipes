58f3874022a2a7b15dbc55b7aa3fba4a1727aef2
Manuel Traut <manut@mecka.net>
2025-11-11 20:06:21 +0100

    Prepare supporting multiple disk-layouts
    Remove everything that can be configured by mkosi from configure.py
    this will ease:
    
      - Supporting multiple disk-layouts in the future
         (rw rootfs vs. a/b with ro /usr)
    
    The configure.py can entirely go away, as soon as users are created
    by gnome-initial-setup
    
    Signed-off-by: Manuel Traut <manut@mecka.net>

--- configure.py
+++ configure.py
@@ -37,16 +37,8 @@ def configure_version(dir_path: pathlib.Path, version: str):
 
 
 def copy_dir(src: pathlib.Path, dst: pathlib.Path) -> pathlib.Path:
-    path = shutil.copytree(src, dst / src.name)
+    path = shutil.copytree(src, dst, dirs_exist_ok=True)
     return pathlib.Path(path)
-
-
-def validate_args(args: argparse.Namespace):
-    if args.architecture == "x86-64" and args.device != "amd64":
-        sys.exit(
-            f"Error: architecutre {args.architecutre} conflicts "
-            f"with device {args.device}",
-        )
 
 
 def parse_args() -> argparse.Namespace:
@@ -54,19 +46,8 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("build_directory", type=pathlib.Path, default="build")
     parser.add_argument("--clean", action="store_true")
 
-    parser.add_argument("--architecture", choices=["x86-64"], default="x86-64")
-    parser.add_argument("--release", choices=["forky"], default="forky")
-    parser.add_argument("--contrib", action="store_true")
-    parser.add_argument("--non_free", action="store_true")
-
-    parser.add_argument("--device", choices=["amd64"], default="amd64")
-
-    parser.add_argument("--hostname", default="phosh")
     parser.add_argument("--username", default="phosh")
     parser.add_argument("--password", default="1234")
-
-    parser.add_argument("--ssh", action="store_true")
-    parser.add_argument("--zram", action="store_true")
 
     parser.add_argument("--version", default="")
 
@@ -77,15 +58,7 @@ def parse_args() -> argparse.Namespace:
 
 def main():
     args = parse_args()
-    validate_args(args)
     options = {key.upper(): val for (key, val) in vars(args).items()}
-
-    repositories = ["main"]
-    if args.contrib:
-        repositories.append("contrib")
-    if args.non_free:
-        repositories.append("non_free")
-    options["REPOSITORIES"] = " ".join(repositories)
 
     if args.build_directory.exists():
         if args.clean:
@@ -99,41 +72,14 @@ def main():
             sys.exit(1)
     args.build_directory.mkdir()
 
-    src = pathlib.Path("./mkosi.conf.d/")
-    dst = args.build_directory / "mkosi.conf.d"
-
-    dst.mkdir()
+    src = pathlib.Path("./mkosi.conf.d")
+    dst = args.build_directory
 
     configure_version(args.build_directory, args.version)
 
-    path = copy_dir(src / "000-init", dst)
+    path = copy_dir(src, dst)
     configure_dir(path, options)
 
-    path = copy_dir(src / "001-device" / args.device, dst)
-    path = pathlib.Path(shutil.move(path, dst / f"001-device-{args.device}"))
-    configure_dir(path, options)
-    if not args.non_free:
-        (path / "mkosi.conf.d" / "001-non-free-packages.conf").unlink()
-
-    path = copy_dir(src / "002-system", dst)
-    configure_dir(path, options)
-
-    path = copy_dir(src / "003-phosh", dst)
-    configure_dir(path, options)
-
-    if args.ssh:
-        path = copy_dir(src / "004-ssh", dst)
-        configure_dir(path, options)
-        # NOTE: 002-system has `etc/tmpfiles.d/chown-home.conf` which shall
-        # change the ownership of `.ssh` directory to user. So just make the
-        # move here.
-        home_src = path / "mkosi.extra" / "home" / "user"
-        home_dst = home_src.with_name(args.username)
-        shutil.move(home_src, home_dst)
-
-    if args.zram:
-        path = copy_dir(src / "004-zram", dst)
-        configure_dir(path, options)
 
     (args.build_directory / "mkosi.cache").mkdir()
 
